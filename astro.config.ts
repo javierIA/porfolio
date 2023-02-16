7027f1c56edc1c0073809ed7697ea13feae7bb31
javierIA <javier.flores@ia.center>
2023-02-16 02:57:13 -0700

    aasa

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-
+site: "https://porfolio-delta-lovat.vercel.app/",
   markdown: {
     shikiConfig: {
       theme: "dracula",

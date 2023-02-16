b8399743dabf0c08135a1d3ecb0d1888cee3976f
javierIA <javier.flores@ia.center>
2023-02-16 02:47:20 -0700

    aa

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "http://localhost:3000",
+    site: "https://porfolio-git-master-javieria.vercel.app/",
   markdown: {
     shikiConfig: {
       theme: "dracula",
